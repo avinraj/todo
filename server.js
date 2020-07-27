40873dec091d342bef398dbbe3ee04653901020a
avinraj <avinraj84@gmail.com>
2020-07-27 18:04:55 +0530

    a

--- server.js
+++ server.js
@@ -3,7 +3,6 @@ const http = require('http');
 const app = express();
 const path = require('path');
 const bodyparser = require('body-parser');
-const mongoose = require('mongoose')
 const UserRoutes = require('./backend/routes/users');
 const TodoRoutes = require('./backend/routes/todo');
 app.use(bodyparser.json());
@@ -11,18 +10,6 @@ app.use(bodyparser.urlencoded({
     extended:false
 }))
 app.use(express.static(__dirname + '/dist/clientSide'));
-mongoose.connect('mongodb://localhost:27017/todolistDB',{
-    useUnifiedTopology:true,
-    useNewUrlParser:true
-})
-.then(()=>{
-    console.log('Database successfully connected')
-},
-error =>{
-    console.log('Database connection failed' +error)
-}
-
-)
 app.use('/user',UserRoutes);
 app.use('/todolist',TodoRoutes);
 app.get('*',(req,res) => {
